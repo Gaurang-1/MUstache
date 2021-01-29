d9ff2c9fe53b235caef360308d019a10c78543a6
Gaurang-1 <72252176+Gaurang-1@users.noreply.github.com>
2021-01-29 14:40:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+nose_x=0;
+nose_y=0;
+
 function preload() {
 
   }
@@ -8,9 +11,21 @@ function preload() {
     video = createCapture(VIDEO);
     video.size(300, 300);
     video.hide();
-  
+    poseNet=ml5.poseNet(video,modal_loaded);
+    poseNet.on('Pose',gotPoses)
   }
 
+  function modal_loaded(){
+    console.log("PoseNet is Initialized");
+  }
+
+  function gotPoses(results){
+    if(results.length >0){
+    console.log(results);
+    console.log("Nose x ="+results[0].pose.nose.x);
+    console.log("Nose y ="+results[0].pose.nose.y);
+    }
+  }
   function draw() {
     image(video, 180, 150, 300, 300);
   }
